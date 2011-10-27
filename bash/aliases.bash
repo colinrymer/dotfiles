2a53a18262507e5b4389802627dfea9ea19fd730
Colin <colin.rymer@gmail.com>
2011-10-27 19:19:36 -0400

    version 1.0 init

---
+++ bash/aliases.bash
@@ -0,0 +1,46 @@
+#-------------------
+# Personal Aliases
+#-------------------
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+# -> Prevents accidentally clobbering files.
+alias mkdir='mkdir -p'
+
+alias bye='logout'
+
+alias sickbeard='python ~/Sick-Beard/SickBeard.py -d'
+
+alias netlogon="sudo sysctl -w net.inet.ip.fw.verbose=1"
+alias netlogoff="sudo sysctl -w net.inet.ip.fw.verbose=0"
+
+alias gemit="rvm gem install"
+
+#-------------------
+# Git Aliases
+#-------------------
+alias push='git push'
+alias pull='git pull'
+alias commit='git commit'
+
+alias ls='ls -A'
+alias ll='ls -l'
+
+alias ..='cd ..'
+alias ~='cd ~'
+alias h='cd ~'
+
+nsm () { nasm -f elf -o "$1".o -l "$1".l "$1".s;}
+
+gi () { sudo gem install "$1" --no-ri --no-rdoc -V; }
+
+#-------------------------------------------------------------
+# spelling typos - highly personal and keyboard-dependent :-)
+#-------------------------------------------------------------
+
+alias xs='cd'
+alias vf='cd'
+alias moer='more'
+alias moew='more'
+alias puhs='push'
---
+++ bash/prompt.bash
@@ -0,0 +1,34 @@
+# Sexy Bash Prompt, inspired by "Extravagant Zsh Prompt"
+# Screenshot: http://img.gf3.ca/d54942f474256ec26a49893681c49b5a.png
+# A big thanks to \amethyst on Freenode
+
+if tput setaf 1 &> /dev/null; then
+    tput sgr0
+    if [[ $(tput colors) -ge 256 ]] 2>/dev/null; then
+      MAGENTA=$(tput setaf 9)
+      ORANGE=$(tput setaf 172)
+      GREEN=$(tput setaf 190)
+      PURPLE=$(tput setaf 141)
+      WHITE=$(tput setaf 254)
+      CLEAR=$(tput setaf 256)
+      RUBY=$(tput setaf 200)
+    else
+      MAGENTA=$(tput setaf 5)
+      ORANGE=$(tput setaf 4)
+      GREEN=$(tput setaf 2)
+      PURPLE=$(tput setaf 1)
+      WHITE=$(tput setaf 7)
+    fi
+    BOLD=$(tput bold)
+    RESET=$(tput sgr0)
+
+fi
+
+parse_git_dirty () {
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+}
+parse_git_branch () {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+}
+
+PS1="\[${MAGENTA}\]\u\[$WHITE\]@\[$ORANGE\]\h \[$WHITE\]in \[$GREEN\]\w\[$WHITE\]\$([[ -n \$(git branch 2> /dev/null) ]] && echo \" on \")\[$PURPLE\]\$(parse_git_branch)\[$RUBY\] using Ruby v. $(ruby -e 'print RUBY_VERSION')\n\[$RESET\]\$ "
