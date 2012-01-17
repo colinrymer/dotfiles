8dde5a975e09552b8ac0c9d77f612ab410c2d28e
Colin <colin.rymer@gmail.com>
2012-01-17 14:17:16 -0500

    added git alias and removed old bashrc function

--- git/aliases.bash
+++ git/aliases.bash
@@ -5,3 +5,14 @@ alias push='git push'
 alias pull='git pull'
 alias commit='git commit'
 alias cam='git commit -a -m'
+
+
+#-------------------
+# Git Functions
+#-------------------
+
+cleanit () {
+	git clean -dxn
+	read -p "Remove those files (y/n)?"
+	[ "$REPLY" == "n" ] || git clean -dxf
+}
