9a625b77155607447b383b097bc299a73d081192
Colin Rymer <colin.rymer@gmail.com>
2014-07-17 12:31:56 -0400

    fix direnv/ls_after_chdir bug

--- zsh/directories.zsh
+++ zsh/directories.zsh
@@ -6,12 +6,6 @@ setopt pushd_ignore_dups
 setopt auto_cd
 cdpath=(. $HOME $HOME/Projects/sites $HOME/Projects/primedia $HOME/Projects)
 
-function ls_after_chpwd() {
-  ls -la .
-}
-
-chpwd_functions=( ls_after_chpwd $chpwd_functions )
-
 # mkdir & cd to it
 function mcd() { 
   mkdir -p "$1" && cd "$1"; 
